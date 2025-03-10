2b6c610e1e6e16d1619930fae99feaae8de4d743
Alex Kulik <bubralubra@gmail.com>
2025-03-10 23:15:13 +0300

    feat: add logic in pure js and html

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+export const SERVER_URL = "ws://localhost:3000";
+
+export const EVENTS = {
+  message: "message",
+  connect: "connection",
+};
--- server/index.js
+++ server/index.js
@@ -1,47 +1,31 @@
 require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const { Server } = require("socket.io");
-const { createServer } = require("node:http");
-const chatRouter = require("./router/chat");
+const ws = require("ws");
 
-const corsOptions = {
-  origin: process.env.REACT_APP_URL,
-  methods: ["GET", "POST"],
-};
+const server = new ws.Server({ port: process.env.SERVER_PORT }, () =>
+  console.log(`WebSocket server starts at port:${process.env.SERVER_PORT}`)
+);
 
-const app = express();
-app.use(cors(corsOptions));
-const server = createServer(app);
-const io = new Server(server, {
-  cors: corsOptions,
-});
+server.on("connection", (webSocket) => {
+  webSocket.on("message", (msg) => {
+    msg = JSON.parse(msg);
+    switch (msg.event) {
+      case "message":
+        console.log(msg);
+        sendMsg(msg);
+        break;
 
-app.use("/chat", chatRouter);
-io.on("connection", (socket) => {
-  console.log("user connected");
+      case "connection":
+        sendMsg(`User ${msg.username} has entered in chat!!!`);
+        break;
 
-  socket.on("message", (msg) => {
-    console.log("Message received: ", msg);
-    io.emit("message", msg);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+      default:
+        break;
+    }
   });
 });
 
-const start = async () => {
-  try {
-    server.listen(process.env.SERVER_PORT, () => {
-      console.log(`server starts at port ${process.env.SERVER_PORT}`);
-    });
-  } catch (error) {
-    console.log(error);
-  }
+const sendMsg = (msg) => {
+  server.clients.forEach((client) => {
+    client.send(JSON.stringify(msg));
+  });
 };
-start();
-
-// server.listen(3000, () => {
-//   console.log('server running at http://localhost:3000');
-// });
