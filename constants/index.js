087e09b1b8fce4bc33c401c608174b1a930445ea
Alex Kulik <bubralubra@gmail.com>
2025-03-11 01:57:53 +0300

    feature: added basic functionality

--- constants/index.js
+++
@@ -1,6 +0,0 @@
-export const SERVER_URL = "ws://localhost:3000";
-
-export const EVENTS = {
-  message: "message",
-  connect: "connection",
-};
--- server/index.js
+++ server/index.js
@@ -1,31 +1,70 @@
 require("dotenv").config();
 const ws = require("ws");
 
+const connectedUsers = new Map();
+
 const server = new ws.Server({ port: process.env.SERVER_PORT }, () =>
   console.log(`WebSocket server starts at port:${process.env.SERVER_PORT}`)
 );
 
+function broadcast(message) {
+  server.clients.forEach((client) => {
+    if (client.readyState === ws.OPEN) {
+      client.send(JSON.stringify(message));
+    }
+  });
+}
+
 server.on("connection", (webSocket) => {
-  webSocket.on("message", (msg) => {
-    msg = JSON.parse(msg);
-    switch (msg.event) {
-      case "message":
-        console.log(msg);
-        sendMsg(msg);
-        break;
+  webSocket.on("message", (dto) => {
+    try {
+      const { message, event, username } = JSON.parse(dto);
 
-      case "connection":
-        sendMsg(`User ${msg.username} has entered in chat!!!`);
-        break;
+      switch (event) {
+        case "message":
+          broadcast({ message, event, username });
+          break;
 
-      default:
-        break;
+        case "connect":
+          connectedUsers.set(webSocket, username);
+          broadcast({
+            currentUsers: Array.from(connectedUsers.values()),
+            event,
+            message: `User ${username} has entered in chat!`,
+          });
+          break;
+
+        case "disconnect":
+          connectedUsers.delete(username);
+          broadcast({
+            currentUsers: Array.from(connectedUsers.values()),
+            event,
+            message: `User ${username} has left the chat!`,
+          });
+        default:
+          console.warn("Unknown event:");
+          break;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  webSocket.on("close", () => {
+    let username;
+    for (const [key, value] of connectedUsers.entries()) {
+      if (key === webSocket) {
+        username = value;
+        connectedUsers.delete(key);
+      }
+    }
+
+    if (username) {
+      broadcast({
+        currentUsers: Array.from(connectedUsers.values()),
+        event: "disconnect",
+        message: `User ${username} has left the chat.`,
+      });
     }
   });
 });
-
-const sendMsg = (msg) => {
-  server.clients.forEach((client) => {
-    client.send(JSON.stringify(msg));
-  });
-};
